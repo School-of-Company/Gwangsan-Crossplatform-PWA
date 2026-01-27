06f03d2ff21f21fc3c0d650e8ce22259e8629392
moonkanghyeon <s25024@gsm.hs.kr>
2026-01-27 23:26:20 +0900

    fix: 웹 플랫폼 폰트 로딩 처리

--- src/shared/assets/fonts/fontLoader.tsx
+++ src/shared/assets/fonts/fontLoader.tsx
@@ -1,10 +1,16 @@
 import * as Font from 'expo-font';
 import { useEffect, useState } from 'react';
+import { Platform } from 'react-native';
 
 export const useCustomFonts = () => {
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
+    if (Platform.OS === 'web') {
+      setLoaded(true);
+      return;
+    }
+
     Font.loadAsync({
       Cafe24SsurroundOTF: require('@/shared/assets/fonts/Cafe24Ssurround-v2.0/Cafe24Ssurround-v2.0.otf'),
     }).then(() => setLoaded(true));
