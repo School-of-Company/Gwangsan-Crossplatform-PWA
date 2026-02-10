68b7397252913ade76e1a7d2772af15a416bfe2d
koreahghg <koreahghg@gmail.com>
2026-02-10 22:40:27 +0900

    refactor: OnboardingPage PWA 형식으로 변환

--- src/view/onboarding/ui/OnboardingPage/index.tsx
+++ src/view/onboarding/ui/OnboardingPage/index.tsx
@@ -1,21 +1,19 @@
-import { View } from 'react-native';
 import { AuthButtonContainer } from '~/entity/onboarding';
 import { OnboardingSlideViewer } from '~/widget/onboarding';
-import { SafeAreaView } from 'react-native-safe-area-context';
 
 const OnboardingPage = () => {
   return (
-    <SafeAreaView className="flex-1 bg-white">
-      <View className="flex-1">
-        <View className="flex-7">
+    <div className="flex-1 bg-white">
+      <div className="flex-1">
+        <div className="flex-7">
           <OnboardingSlideViewer />
-        </View>
-        <View className="flex-1" />
-        <View className="flex-2">
+        </div>
+        <div className="flex-1" />
+        <div className="flex-2">
           <AuthButtonContainer />
-        </View>
-      </View>
-    </SafeAreaView>
+        </div>
+      </div>
+    </div>
   );
 };
 
