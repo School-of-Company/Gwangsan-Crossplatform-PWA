e8a7f218327cf09bca9cac004e0745a6bfa7711e
moonkanghyeon <s25024@gsm.hs.kr>
2026-02-12 22:08:03 +0900

    refactor: 불필요한 div 삭제

--- src/entity/auth/ui/SignupForm/index.tsx
+++ src/entity/auth/ui/SignupForm/index.tsx
@@ -26,27 +26,25 @@ export default function SignupForm({
 
   return (
     <div className="flex min-h-screen flex-col bg-white">
-      <div className="flex flex-1 flex-col">
-        <div className="flex flex-1 flex-col gap-8 px-6">
-          <div className="flex flex-row items-center pt-4">
-            <button className="flex flex-row items-center" onClick={onBack || prevStep}>
-              <BackArrow />
-              <span className="ml-2 text-gray-500">뒤로</span>
-            </button>
-          </div>
+      <div className="flex flex-1 flex-col gap-8 px-6">
+        <div className="flex flex-row items-center pt-4">
+          <button className="flex flex-row items-center" onClick={onBack || prevStep}>
+            <BackArrow />
+            <span className="ml-2 text-gray-500">뒤로</span>
+          </button>
+        </div>
 
-          <div>
-            <h1 className="text-3xl font-bold">{title}</h1>
-            <p className="mt-4 text-lg text-gray-700">{description}</p>
-          </div>
+        <div>
+          <h1 className="text-3xl font-bold">{title}</h1>
+          <p className="mt-4 text-lg text-gray-700">{description}</p>
+        </div>
 
-          <div className="mt-8 flex-1">{children}</div>
+        <div className="mt-8 flex-1">{children}</div>
 
-          <div className="mb-8 mt-auto">
-            <Button onPress={onNext} disabled={isNextDisabled}>
-              {nextButtonText}
-            </Button>
-          </div>
+        <div className="mb-8 mt-auto">
+          <Button onPress={onNext} disabled={isNextDisabled}>
+            {nextButtonText}
+          </Button>
         </div>
       </div>
     </div>
