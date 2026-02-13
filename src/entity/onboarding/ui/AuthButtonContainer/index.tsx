bdea89f8ae3c25d61f0f7ac1aaaeb4c61257f8f0
koreahghg <koreahghg@gmail.com>
2026-02-13 23:41:40 +0900

    refactor: AuthButtonContainer PWA 형식으로 변환

--- src/entity/onboarding/ui/AuthButtonContainer/index.tsx
+++ src/entity/onboarding/ui/AuthButtonContainer/index.tsx
@@ -1,20 +1,19 @@
-import { Text, View } from 'react-native';
 import { Button } from '~/shared/ui/Button';
 import { router } from 'expo-router';
 
 const AuthButtonContainer = () => {
   return (
-    <View className="flex flex-col gap-3 px-6 ">
+    <div className="flex flex-col gap-3 px-6">
       <Button onPress={() => router.push('/signin')}>로그인</Button>
       <Button variant="secondary" onPress={() => router.push('/signup')}>
         회원가입
       </Button>
-      <Text
+      <button
         className="text-center text-sm text-gray-500 underline"
-        onPress={() => router.push('/resetPassword')}>
-        비밀번호 변경하기{' '}
-      </Text>
-    </View>
+        onClick={() => router.push('/resetPassword')}>
+        비밀번호 변경하기
+      </button>
+    </div>
   );
 };
 
