d841216b477e66d694c0439523965c9eca2a0040
koreahghg <koreahghg@gmail.com>
2026-02-14 10:29:30 +0900

    fix: flex-col 클래스 추가

--- src/view/onboarding/ui/OnboardingPage/index.tsx
+++ src/view/onboarding/ui/OnboardingPage/index.tsx
@@ -4,7 +4,7 @@ import { OnboardingSlideViewer } from '~/widget/onboarding';
 const OnboardingPage = () => {
   return (
     <div className="flex flex-1 flex-col bg-white">
-      <div className="flex flex-1">
+      <div className="flex flex-col flex-1">
         <div className="flex-7 flex">
           <OnboardingSlideViewer />
         </div>
