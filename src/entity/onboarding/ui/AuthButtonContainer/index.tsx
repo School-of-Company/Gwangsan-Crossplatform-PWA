30a5d8c9d568e456eda1eb3a3b9c96ca3bfa2a2c
koreahghg <koreahghg@gmail.com>
2026-02-14 10:32:14 +0900

    refactor: Link 컴포넌트 사용

--- src/entity/onboarding/ui/AuthButtonContainer/index.tsx
+++ src/entity/onboarding/ui/AuthButtonContainer/index.tsx
@@ -1,5 +1,5 @@
 import { Button } from '~/shared/ui/Button';
-import { router } from 'expo-router';
+import { router, Link } from 'expo-router';
 
 const AuthButtonContainer = () => {
   return (
@@ -8,11 +8,9 @@ const AuthButtonContainer = () => {
       <Button variant="secondary" onPress={() => router.push('/signup')}>
         회원가입
       </Button>
-      <button
-        className="text-center text-sm text-gray-500 underline"
-        onClick={() => router.push('/resetPassword')}>
+      <Link href="/resetPassword" className="text-center text-sm text-gray-500 underline">
         비밀번호 변경하기
-      </button>
+      </Link>
     </div>
   );
 };
