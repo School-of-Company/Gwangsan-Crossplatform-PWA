08a519b8c379ef631010acec5e2e0733eb0ece00
koreahghg <koreahghg@gmail.com>
2026-02-14 10:48:47 +0900

    refactor: flex 구조 단순화

--- src/view/onboarding/ui/OnboardingPage/index.tsx
+++ src/view/onboarding/ui/OnboardingPage/index.tsx
@@ -4,14 +4,12 @@ import { OnboardingSlideViewer } from '~/widget/onboarding';
 const OnboardingPage = () => {
   return (
     <div className="flex flex-1 flex-col bg-white">
-      <div className="flex flex-1 flex-col">
-        <div className="flex-7 flex">
-          <OnboardingSlideViewer />
-        </div>
-        <div className="flex flex-1" />
-        <div className="flex-2 flex">
-          <AuthButtonContainer />
-        </div>
+      <div className="flex-7 flex">
+        <OnboardingSlideViewer />
+      </div>
+      <div className="flex flex-1" />
+      <div className="flex-2 flex">
+        <AuthButtonContainer />
       </div>
     </div>
   );
