0b0edaf3100a634a2d7ae5a04fc82cbe4f640749
koreahghg <koreahghg@gmail.com>
2026-02-18 13:32:14 +0900

    refactor: MainPage PWA 형식으로 변환

--- src/view/main/ui/MainPage/index.tsx
+++ src/view/main/ui/MainPage/index.tsx
@@ -2,16 +2,15 @@ import Header from '~/entity/main/ui/header';
 import { Footer } from '~/shared/ui/Footer';
 import { Inform, MainSlideViewer } from '~/widget/main';
 import { useGetMyInformation } from '../../../../entity/main/model/useGetMyInformation';
-import Toast from 'react-native-toast-message';
+import { toast } from 'react-toastify';
 
 export default function MainPageView() {
   const { data, isError, error } = useGetMyInformation();
-  if (isError)
-    Toast.show({
-      type: 'error',
-      text1: '정보 조회 실패',
-      text2: error instanceof Error ? error.message : '알 수 없는 오류가 발생했습니다.',
-    });
+  if (isError) {
+    toast.error(
+      `정보 조회 실패: ${error instanceof Error ? error.message : '알 수 없는 오류가 발생했습니다.'}`
+    );
+  }
   return (
     <div className="flex-1 bg-white">
       <Header />
