f3f7892fbd59a27a956341189e9ca8085462b1a3
koreahghg <koreahghg@gmail.com>
2026-02-18 13:13:12 +0900

    refactor: 불필요한 div 제거

--- src/view/onboarding/ui/OnboardingPage/index.tsx
+++ src/view/onboarding/ui/OnboardingPage/index.tsx
@@ -4,14 +4,12 @@ import { OnboardingSlideViewer } from '~/widget/onboarding';
 const OnboardingPage = () => {
   return (
     <div className="flex h-dvh w-full flex-1 flex-col bg-white">
-      <div className="flex flex-1 flex-col">
-        <div className="flex-[7]">
-          <OnboardingSlideViewer />
-        </div>
-        <div className="flex-1" />
-        <div className="flex-none">
-          <AuthButtonContainer />
-        </div>
+      <div className="flex-[7]">
+        <OnboardingSlideViewer />
+      </div>
+      <div className="flex-1" />
+      <div className="flex-none">
+        <AuthButtonContainer />
       </div>
     </div>
   );
