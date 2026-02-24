252f85a179d5265fdeea3a2a24d475d51b39de09
koreahghg <koreahghg@gmail.com>
2026-02-24 17:39:29 +0900

    refactor: useUploadImage에 react toastify 사용

--- src/shared/model/useUploadImage.ts
+++ src/shared/model/useUploadImage.ts
@@ -1,26 +1,16 @@
 import { useMutation } from '@tanstack/react-query';
 import { uploadImage } from '../api/uploadImage';
 import { ImageType } from '../types/imageType';
-import Toast from 'react-native-toast-message';
+import { toast } from 'react-toastify';
 
 export const useUploadImage = () => {
-  return useMutation<ImageType, Error, string>({
-    mutationFn: (uri: string) => uploadImage(uri),
+  return useMutation<ImageType, Error, File | string>({
+    mutationFn: (input: File | string) => uploadImage(input),
     onSuccess: () => {
-      Toast.show({
-        type: 'success',
-        text1: '이미지 업로드 성공',
-        text2: '이미지가 성공적으로 업로드되었습니다.',
-        visibilityTime: 2000,
-      });
+      toast.success('이미지가 성공적으로 업로드되었습니다.');
     },
     onError: (error) => {
-      Toast.show({
-        type: 'error',
-        text1: '이미지 업로드 실패',
-        text2: error.message || '이미지 업로드 중 오류가 발생했습니다.',
-        visibilityTime: 3000,
-      });
+      toast.error(error.message || '이미지 업로드 중 오류가 발생했습니다.');
     },
   });
 };
