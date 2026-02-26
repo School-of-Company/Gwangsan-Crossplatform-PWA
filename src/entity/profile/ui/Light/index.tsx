c4866f0efbb08e4e7e612069bc633b7ed112dfd3
moonkanghyeon <s25024@gsm.hs.kr>
2026-02-26 18:04:48 +0900

    refactor: Light 컴포넌트를 웹 HTML로 마이그레이션

--- src/entity/profile/ui/Light/index.tsx
+++ src/entity/profile/ui/Light/index.tsx
@@ -1,5 +1,4 @@
 import { clsx } from 'clsx';
-import { Text, View } from 'react-native';
 
 interface LightProps {
   lightLevel?: number;
@@ -7,19 +6,21 @@ interface LightProps {
 
 export default function Light({ lightLevel = 1 }: LightProps) {
   return (
-    <View className="px-6 py-10">
-      <Text className="mb-6 text-titleSmall">밝기</Text>
+    <div className="px-6 py-10">
+      <h3 className="mb-6 text-titleSmall">밝기</h3>
 
-      <View className="relative flex h-5 w-full justify-center rounded-xl bg-gray-200">
-        <View className="absolute left-1 right-1 h-3 overflow-hidden rounded-xl">
-          <View
+      <div className="relative flex h-5 w-full justify-center rounded-xl bg-gray-200">
+        <div className="absolute left-1 right-1 h-3 overflow-hidden rounded-xl">
+          <div
             className={clsx('h-full rounded-xl')}
             style={{ width: `${Math.min(Math.max(lightLevel, 0), 100)}%` }}
           />
-        </View>
-      </View>
+        </div>
+      </div>
 
-      <Text className="ml-auto">{Math.ceil(lightLevel / 10)}단계</Text>
-    </View>
+      <div className="flex justify-end">
+        <span>{Math.ceil(lightLevel / 10)}단계</span>
+      </div>
+    </div>
   );
 }
