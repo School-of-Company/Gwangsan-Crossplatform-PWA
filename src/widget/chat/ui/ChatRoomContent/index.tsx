62ff2e4ce213b990248bbcac8690dc0298a83d24
koreahghg <koreahghg@gmail.com>
2026-03-03 12:27:46 +0900

    faet: 줄바꿈 PWA로 변환

--- src/widget/chat/ui/ChatRoomContent/index.tsx
+++ src/widget/chat/ui/ChatRoomContent/index.tsx
@@ -107,7 +107,7 @@ export const ChatRoomContent: React.FC<ChatRoomContentProps> = ({
         <path d="M21 15C21 15.5304 20.7893 16.0391 20.4142 16.4142C20.0391 16.7893 19.5304 17 19 17H7L3 21V5C3 4.46957 3.21071 3.96086 3.58579 3.58579C3.96086 3.21071 4.46957 3 5 3H19C19.5304 3 20.0391 3.21071 20.4142 3.58579C20.7893 3.96086 21 4.46957 21 5V15Z" stroke="#D1D5DB" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
       </svg>
       <p className="mt-4 text-center text-gray-500">
-        아직 대화가 없습니다.{'\n'}첫 메시지를 보내보세요!
+        아직 대화가 없습니다.<br />첫 메시지를 보내보세요!
       </p>
     </div>
   );
